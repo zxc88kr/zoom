1d90bf7c3a34f0015e0db450af3d2bce75ea00db
zxc88kr <zxc88kr@naver.com>
2023-01-27 00:15:52 +0900

    SOCKETIO

--- src/server.js
+++ src/server.js
@@ -42,7 +42,6 @@ function countRoom(roomName) {
 wsServer.on("connection", (socket) => {
     socket["nickname"] = "Anonymous";
     socket.onAny((event) => {
-        console.log(wsServer.sockets.adapter);
         console.log(`Socket Event: ${event}`);
     });
     socket.on("disconnecting", () => {
@@ -71,26 +70,3 @@ wsServer.on("connection", (socket) => {
 httpServer.listen(3000, () => {
     console.log(`Listening on http://localhost:3000`);
 });
-
-/* import WebSocket from "ws";
-const wss = new WebSocket.Server({ server });
-const sockets = [];
-wss.on("connection", (socket) => {
-    sockets.push(socket);
-    socket["nickname"] = "Anonymous";
-    console.log("Connected to Browser ✅");
-    socket.on("close", () => {
-        console.log("Disconnected from the Browser ❌");
-    });
-    socket.on("message", (msg) => {
-        const message = JSON.parse(msg);
-        switch (message.type) {
-            case "message":
-                sockets.forEach((aSocket) => {
-                    aSocket.send(`${socket.nickname}: ${message.payload}`);
-                }); break;
-            case "nickname":
-                socket["nickname"] = message.payload;
-        }
-    });
-}); */
